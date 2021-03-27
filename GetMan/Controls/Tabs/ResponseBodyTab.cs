121b5f0e2b60325092e1e5aab1dd679b8396f780
mkbmain <michael.bourke@live.co.uk>
2021-03-27 21:56:45 +0000

    format json if its json

--- GetMan/Controls/Tabs/ResponseBodyTab.cs
+++ GetMan/Controls/Tabs/ResponseBodyTab.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Drawing;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace GetMan.Controls.Tabs
 {
     public class ResponseBodyTab : BaseTabPanel
     {
+        private readonly TextBox _bodyTextBox = new TextBox
+            {Location = new Point(1, 35), AutoSize = false, Multiline = true};
 
-        private readonly TextBox _bodyTextBox = new TextBox {Location = new Point(1, 35), AutoSize = false, Multiline = true};
         private readonly Label _result = new Label {Location = new Point(1, 1), AutoSize = true};
-        public ResponseBodyTab(Size tabArea) 
+
+        public ResponseBodyTab(Size tabArea)
         {
             this.Controls.Add(_bodyTextBox);
             this.Controls.Add(_result);
@@ -16,18 +20,30 @@ namespace GetMan.Controls.Tabs
             _bodyTextBox.ScrollBars = ScrollBars.Both;
         }
 
-        public void Set(string body,string status)
+        public void Set(string body, string status)
         {
-            _bodyTextBox.Text = body;
+            string json = body;
+            try
+            {
+                var b = Newtonsoft.Json.JsonConvert.DeserializeObject(body);
+                if (b != null)
+                {
+                    json = Newtonsoft.Json.JsonConvert.SerializeObject(b, Formatting.Indented);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+
+            _bodyTextBox.Text = json;
             _result.Text = status;
         }
-        
-        
+
 
         public sealed override void InnerResize(Size tabArea)
         {
             _bodyTextBox.Location = new Point(1, _result.Bottom + 15);
-            _bodyTextBox.Size = new Size(tabArea.Width-10, tabArea.Height-_result.Height-15);
+            _bodyTextBox.Size = new Size(tabArea.Width - 10, tabArea.Height - _result.Height - 15);
             base.InnerResize(tabArea);
         }
     }
